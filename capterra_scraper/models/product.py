1669c460d88bbbc29c7f394ff8ed96b2d369e772
Ahsan <ahsantahir0@gmail.com>
2023-05-01 14:05:00 +0500

    feat: add Product dataclass with field definitions

--- capterra_scraper/models/product.py
+++ capterra_scraper/models/product.py
@@ -24,3 +24,38 @@ class CategoryInfo:
         return asdict(self)
 
 
+@dataclass
+class Product:
+    """Represents a single software product scraped from Capterra."""
+
+    category_url: str = ""
+    category_id: Optional[int] = None
+    category_name: str = ""
+    product_id: Optional[int] = None
+    name: str = ""
+    website: str = ""
+    rating: Optional[float] = None
+    review_count: int = 0
+    capterra_url: str = ""
+    vendor_name: str = ""
+    description: str = ""
+    long_description: str = ""
+    year_founded: Optional[int] = None
+    value_for_money_rating: Optional[float] = None
+    customer_service_rating: Optional[float] = None
+    ease_of_use_rating: Optional[float] = None
+    recommendation_rating: Optional[float] = None
+    functionality_rating: Optional[float] = None
+    pricing_overview: str = ""
+    pricing: str = ""
+    location: str = ""
+    has_free_trial: bool = False
+    training: List[str] = field(default_factory=list)
+    support: List[str] = field(default_factory=list)
+    best_for: str = ""
+    created_at: str = ""
+    updated_at: str = ""
+
+    def to_dict(self) -> Dict[str, Any]:
+        return asdict(self)
+
