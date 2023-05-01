d86a9430f0bb2403f7579fef3255826b77f99b67
Ahsan <ahsantahir0@gmail.com>
2023-05-01 11:08:00 +0500

    feat: add CookieServiceConfig dataclass

--- capterra_scraper/config.py
+++ capterra_scraper/config.py
@@ -25,3 +25,24 @@ class ProxyConfig:
         )
 
 
+@dataclass(frozen=True)
+class CookieServiceConfig:
+    """Settings for the cookie generation API."""
+
+    url: str = "https://api.parallaxsystems.io/gen"
+    auth_token: str = ""
+    site: str = "capterra"
+    proxy_region: str = "eu"
+    region: str = "com"
+
+    @classmethod
+    def from_env(cls) -> "CookieServiceConfig":
+        """Build cookie-service config from environment variables."""
+        return cls(
+            url=os.environ.get("CAPTERRA_COOKIE_API_URL", "https://api.parallaxsystems.io/gen"),
+            auth_token=os.environ.get("CAPTERRA_COOKIE_AUTH", ""),
+            proxy_region=os.environ.get("CAPTERRA_COOKIE_PROXY_REGION", "eu"),
+            region=os.environ.get("CAPTERRA_COOKIE_REGION", "com"),
+        )
+
+
