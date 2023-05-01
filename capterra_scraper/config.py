44f2dc24946ecad7ac684d955891797eb00e6548
Ahsan <ahsantahir0@gmail.com>
2023-05-01 11:35:00 +0500

    feat: add default headers and scraper constants

--- capterra_scraper/config.py
+++ capterra_scraper/config.py
@@ -46,3 +46,30 @@ class CookieServiceConfig:
         )
 
 
+BASE_URL = "https://www.capterra.com"
+
+DEFAULT_HEADERS: Dict[str, str] = {
+    "authority": "www.capterra.com",
+    "accept": (
+        "text/html,application/xhtml+xml,application/xml;q=0.9,"
+        "image/avif,image/webp,image/apng,*/*;q=0.8"
+    ),
+    "accept-language": "en-US,en;q=0.8",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "sec-gpc": "1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/114.0.0.0 Safari/537.36"
+    ),
+}
+
+MAX_RETRIES: int = 5
+DEFAULT_THREADS: int = 4
+MAX_THREAD_CAP: int = 8
