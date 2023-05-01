25422fb6c57e69bae7466f9ba6c1c12994ae2b44
Ahsan <ahsantahir0@gmail.com>
2023-05-01 15:15:00 +0500

    feat: implement Product.from_ssr_data parser

--- capterra_scraper/models/product.py
+++ capterra_scraper/models/product.py
@@ -59,3 +59,50 @@ class Product:
     def to_dict(self) -> Dict[str, Any]:
         return asdict(self)
 
+    @classmethod
+    def from_ssr_data(cls, raw: Dict[str, Any]) -> "Product":
+        """Parse a product from Capterra's ``SSR_BRIDGE_DATA`` JSON payload."""
+        category = raw.get("category", {})
+        vendor = raw.get("vendor", {})
+
+        category_html = category.get("htmlName", "")
+        if not category_html.startswith("/"):
+            category_html = "/" + category_html
+        category_url = "https://www.capterra.com" + category_html
+
+        product_path = raw.get("productUrl", "")
+        if not product_path.startswith("/"):
+            product_path = "/" + product_path
+        capterra_url = "https://www.capterra.com" + product_path
+
+        now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+        return cls(
+            category_url=category_url,
+            category_id=category.get("id"),
+            category_name=category.get("longName", ""),
+            product_id=raw.get("productId"),
+            name=raw.get("name", ""),
+            website=vendor.get("url", ""),
+            rating=raw.get("overallRating"),
+            review_count=raw.get("reviewsTotal", 0),
+            capterra_url=capterra_url,
+            vendor_name=vendor.get("name", ""),
+            description=raw.get("shortDescription", ""),
+            long_description=raw.get("longDescription", ""),
+            year_founded=vendor.get("yearFounded"),
+            value_for_money_rating=raw.get("valueForMoneyRating"),
+            customer_service_rating=raw.get("customerServiceRating"),
+            ease_of_use_rating=raw.get("easeOfUseRating"),
+            recommendation_rating=raw.get("recommendationRating"),
+            functionality_rating=raw.get("functionalityRating"),
+            pricing_overview=raw.get("pricingDetails", ""),
+            pricing=raw.get("pricing", ""),
+            location=vendor.get("location", ""),
+            has_free_trial=raw.get("hasFreeTrial", False),
+            training=[item["name"] for item in raw.get("training", [])],
+            support=[item["name"] for item in raw.get("support", [])],
+            best_for=raw.get("bestFor", ""),
+            created_at=now,
+            updated_at=now,
+        )
