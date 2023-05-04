c120bb86a592bc31f181dd84ede3a03b75d25fba
Ahsan <ahsantahir0@gmail.com>
2023-05-04 13:10:00 +0500

    feat: add product URL pagination per category

--- capterra_scraper/spiders/category_spider.py
+++ capterra_scraper/spiders/category_spider.py
@@ -55,3 +55,70 @@ def fetch_category_urls(client: CapterraHttpClient, cookie: Optional[str] = None
     return urls
 
 
+def fetch_product_urls_for_category(
+    client: CapterraHttpClient,
+    category_path: str,
+    cookie: Optional[str] = None,
+) -> Optional[CategoryInfo]:
+    """Paginate through a category listing and collect all product URLs.
+
+    Returns a :class:`CategoryInfo` with the product URL list, or *None*
+    if the category could not be scraped.
+    """
+    page = 1
+    product_urls: List[str] = []
+    category_name = ""
+    capterra_count = 0
+    category_url = ""
+
+    while True:
+        endpoint = f"{category_path}?page={page}"
+        resp = _fetch_with_retries(client, endpoint, cookie)
+
+        if resp is None or resp.status_code != 200:
+            break
+
+        soup = BeautifulSoup(resp.text, "html.parser")
+
+        if page == 1:
+            category_url = endpoint
+            count_div = soup.find("div", {"data-testid": "amount-of-products"})
+            if count_div:
+                try:
+                    capterra_count = int(
+                        count_div.text.split("(")[1].split(")")[0].strip()
+                    )
+                except (IndexError, ValueError):
+                    capterra_count = 0
+
+            h1 = soup.find("h1", {"class": "text-xl font-bold md:text-3xl"})
+            category_name = h1.text if h1 else ""
+
+        cards = soup.find_all("div", {"class": "p-md"})
+        page_urls = [div.find("a")["href"] for div in cards if div.find("a")]
+
+        if not page_urls:
+            break
+
+        product_urls.extend(page_urls)
+        page += 1
+
+    if not product_urls:
+        return None
+
+    unique_urls = list(set(product_urls))
+    logger.info(
+        "Category '%s': found %d products (Capterra reports %d)",
+        category_name,
+        len(unique_urls),
+        capterra_count,
+    )
+
+    return CategoryInfo(
+        name=category_name,
+        url=category_url,
+        capterra_count=capterra_count,
+        product_urls=unique_urls,
+    )
+
+
