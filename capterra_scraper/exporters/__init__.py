d914d249dbeae4bab326749a8d5c3c354fcc0fee
Ahsan <ahsantahir0@gmail.com>
2023-05-22 13:10:00 +0500

    refactor: add public API exports to exporters package

--- capterra_scraper/exporters/__init__.py
+++ capterra_scraper/exporters/__init__.py
@@ -1,1 +1,6 @@
 """Data export utilities."""
+
+from capterra_scraper.exporters.csv_exporter import export_products_csv
+from capterra_scraper.exporters.json_exporter import export_products_json, merge_json_files
+
+__all__ = ["export_products_csv", "export_products_json", "merge_json_files"]
