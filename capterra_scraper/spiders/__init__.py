ee369ba5f7f06f36d944b35ced6d207b6e782d6c
Ahsan <ahsantahir0@gmail.com>
2023-05-22 13:45:00 +0500

    refactor: add public API exports to spiders package

--- capterra_scraper/spiders/__init__.py
+++ capterra_scraper/spiders/__init__.py
@@ -1,1 +1,6 @@
 """Web spiders for crawling Capterra."""
+
+from capterra_scraper.spiders.category_spider import fetch_category_urls
+from capterra_scraper.spiders.product_spider import extract_product
+
+__all__ = ["fetch_category_urls", "extract_product"]
